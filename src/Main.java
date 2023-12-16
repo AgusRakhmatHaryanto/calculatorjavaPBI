8630711b34a68ca5728d14a843439ab7588b10a7
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2023-12-16 15:40:40 +0700

    [agus]fix : add do while

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,33 @@ import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        char pilihan;
 
-        System.out.print("Masukkan Angka 1: ");
-        int a = scanner.nextInt();
+        do {
+            System.out.print("Masukkan Angka 1: ");
+            int a = scanner.nextInt();
 
-        System.out.print("Masukkan Angka 2: ");
-        int b = scanner.nextInt();
+            System.out.print("Masukkan Angka 2: ");
+            int b = scanner.nextInt();
 
-        System.out.println("Angka 1 : " + a + " dan " + "Angka 2 : " + b);
+            System.out.println("Angka 1 : " + a + " dan " + "Angka 2 : " + b);
 
-        System.out.println("Penjumlahan: " + Calculator.tambah(a, b));
-        System.out.println("Pengurangan: " + Calculator.kurang(a, b));
-        System.out.println("Perkalian: " + Calculator.kali(a, b));
+            System.out.println("Penjumlahan: " + Calculator.tambah(a, b));
+            System.out.println("Pengurangan: " + Calculator.kurang(a, b));
+            System.out.println("Perkalian: " + Calculator.kali(a, b));
 
-        try {
-            double hasilBagi = Calculator.bagi(a, b);
-            DecimalFormat df = new DecimalFormat("#.##");
-            System.out.println("Pembagian: " + df.format(hasilBagi));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
+            try {
+                double hasilBagi = Calculator.bagi(a, b);
+                DecimalFormat df = new DecimalFormat("#.##");
+                System.out.println("Pembagian: " + df.format(hasilBagi));
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print("Apakah Anda ingin menghitung lagi? (y/n): ");
+            pilihan = scanner.next().charAt(0);
+
+        } while (pilihan == 'y' || pilihan == 'Y');
 
         // Tutup Scanner setelah digunakan
         scanner.close();
